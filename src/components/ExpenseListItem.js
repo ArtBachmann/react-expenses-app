f88238d2256f5b8a3057a5c29bba43549152f9b8
Art <bachmann.art@gmail.com>
2018-08-05 10:34:37 +0300

    Remove button and action added

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import ExpenseList from './ExpenseList';
+import { removeExpense } from '../actions/expenses';
+import connect from '../../node_modules/react-redux/lib/connect/connect';
 
-const ExpenseListItem = ({ description, amount, createdAt }) => {
+const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => {
   return (
     <div>
       <h3>{description}</h3>
       <h5>{amount} - {createdAt}</h5>
+      <button onClick={() => {
+        dispatch(removeExpense({ id }));
+      }}>Remove</button>
     </div>
   )
 }
 
-export default ExpenseListItem;
+export default connect()(ExpenseListItem);
